f32626231e7b01378e30720be83f6d9711d12637
Ilya.Chernov <ilya.Chernov@waveaccess.ru>
2024-03-29 14:00:41 +0300

    https -> http

--- ollama_old/service_flask.py
+++ ollama_old/service_flask.py
@@ -1,12 +1,10 @@
 import requests
 from flask import Flask, request, jsonify
-from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 
 
 @app.route("/", methods=['POST', 'OPTIONS'])
-@cross_origin(origins='*')
 def process_request():
     headers = {'Content-type': 'application/json; charset=UTF-8'}
     ollama_req = requests.post("http://10.11.15.67:11434/api/generate", headers=headers, data=request.data)
@@ -16,4 +14,4 @@ def process_request():
 
 
 if __name__ == "__main__":
-    app.run(port=4443, host='0.0.0.0', ssl_context=('cert.pem', 'key.pem'))
+    app.run(port=4443, host='0.0.0.0')

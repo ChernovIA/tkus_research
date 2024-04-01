8e886ac356276ac515140d6b708e6874ab14ac7e
Ilya.Chernov <ilya.Chernov@waveaccess.ru>
2024-04-01 17:18:48 +0300

    deleted unused file

--- model/sql_answer.py
+++
@@ -1,8 +0,0 @@
-from langchain_core.pydantic_v1 import BaseModel, Field
-
-
-class SqlAnswer(BaseModel):
-    question: str = Field(description="Question here")
-    sql_to_run: str = Field(description="SQL Query to run")
-    sql_result: str = Field(description="Result of the SQLQuery")
-    answer: str = Field(description="Final answer here")

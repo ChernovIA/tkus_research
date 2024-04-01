5b59e89566426c10388193838b5a4db727866d56
Ilya.Chernov <ilya.Chernov@waveaccess.ru>
2024-04-01 14:53:48 +0300

    added cors

--- ollama_old/service_flask.py
+++ ollama_old/service_flask.py
@@ -1,10 +1,12 @@
 import requests
 from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 
 
 @app.route("/", methods=['POST', 'OPTIONS'])
+@cross_origin(origins='*')
 def process_request():
     headers = {'Content-type': 'application/json; charset=UTF-8'}
     ollama_req = requests.post("http://10.11.15.67:11434/api/generate", headers=headers, data=request.data)

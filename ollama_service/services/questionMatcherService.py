0c45803c5ea4194e1db7e8b2337ed60481c8564a
Ilya.Fisenko <ilya.fisenko@waveaccess.global>
2024-04-02 14:57:59 +0400

    check query filter string

--- ollama_service/services/questionMatcherService.py
+++ ollama_service/services/questionMatcherService.py
@@ -13,17 +13,20 @@ class QuestionMatcher:
             ORDER BY rank DESC limit 1;
         '''
 
-
     def match(self, question: str) -> QuestionMatherAnswer | None:
         df = db_service.run_query_pandas(self.query.format(question=question))
 
         if not df.empty:
             result = df.iloc[0]
-            sql_query_for_filter = result['filter']
-            filter_words = db_service.run_query_pandas(sql_query_for_filter)['value'].tolist()
+            sql_query_for_filter = str(result['filter'])
 
-            filtered = [s for s in filter_words if s in question]
+            if len(sql_query_for_filter):
+                filter_words = db_service.run_query_pandas(sql_query_for_filter)['value'].tolist()
+                filtered = [s for s in filter_words if s in question]
+            else:
+                filtered = []
 
-            return QuestionMatherAnswer(result['id'], question, result['prompt'], [filtered, filter_words], result['query'])
+
+            return QuestionMatherAnswer(result['id'], question, result['prompt'], filtered, result['query'])
         else:
             return None

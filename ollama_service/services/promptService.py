30c18e9189bc3354ae91aa061fa7fdfcdddd3e90
Ilya.Chernov <ilya.Chernov@waveaccess.ru>
2024-04-02 14:00:35 +0300

    added 3 answer

--- ollama_service/services/promptService.py
+++ ollama_service/services/promptService.py
@@ -23,15 +23,28 @@ def process_q1(result: Sequence[Row[Any]]):
     return db_context
 
 
+def process_q2(result: Sequence[Row[Any]]):
+    db_context = ""
+    for row in result:
+        db_context = f"{row[0]} had {row[1]}"
+    return db_context
+
+
 class PromptService:
     def __init__(self):
         self.process_map = {
             0: process_q0,
-            1: process_q1
+            1: process_q1,
+            2: process_q2,
+            3: process_q2,
+            4: process_q2
         }
 
-    def obtain_prompt(self, id: int, query: str, query_filter: str):
-        sql_query = query.format(f"'{','.join(query_filter)}'")
+    def obtain_prompt(self, id: int, query: str, query_filter: str | None):
+        if query_filter is not None:
+            sql_query = query.format(f"'{','.join(query_filter)}'")
+        else:
+            sql_query = query
         result = db_service.run_query(sql_query)
         db_context = self.process_map[id](result)
 

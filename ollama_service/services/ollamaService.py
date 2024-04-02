878da97e5727b69c5c172cee012eb51b0e26a3af
Ilya.Chernov <ilya.Chernov@waveaccess.ru>
2024-04-02 15:58:00 +0300

    added random answer

--- ollama_service/services/ollamaService.py
+++ ollama_service/services/ollamaService.py
@@ -1,12 +1,13 @@
 
 
-from langchain_core.prompt_values import PromptValue
+from langchain_core.prompts import ChatPromptTemplate
 from langchain_community.llms import Ollama
 
 from pref.envHolder import EnvHolder
 from services.promptService import PromptService
 
 from models.questionMatherAnswer import QuestionMatherAnswer
+
 
 envHolder = EnvHolder()
 promptService = PromptService()
@@ -17,9 +18,23 @@ class OllamaService:
         ollama_base_url = f'http://{envHolder.server_ip}:{envHolder.ollama_port}'
         self.ollama = Ollama(base_url=ollama_base_url, model="llama2:7b-chat-q4_0")
 
-    def generate(self, payload: QuestionMatherAnswer) -> PromptValue:
+    def generate(self, payload: QuestionMatherAnswer) -> str:
         prompt_template = promptService.obtain_prompt(payload.id, payload.query, payload.filter)
 
         chain = prompt_template | self.ollama
         result = chain.invoke({"input": payload.question})
+        return result
+
+    def generate_random(self, question: str) -> str:
+        system_message = "You do not understand question, so say joke about this"
+        human_qry_template = """
+            Input:
+            {input}"""
+        messages = [
+            ("system", system_message),
+            ("human", human_qry_template)
+        ]
+        prompt_template = ChatPromptTemplate.from_messages(messages)
+        chain = prompt_template | self.ollama
+        result = chain.invoke({"input": question})
         return result

2f4bbc0ae652eb9ad433e2dc6ecb6ef6ad0cb47c
Ilya.Chernov <ilya.Chernov@waveaccess.ru>
2024-04-02 11:41:39 +0300

    added first method process

--- ollama_service/services/promptService.py
+++ ollama_service/services/promptService.py
@@ -1,3 +1,4 @@
+from decimal import Decimal
 from typing import Sequence, Any
 
 from langchain_core.prompts import ChatPromptTemplate
@@ -8,53 +9,39 @@ from services.dbService import DBService
 
 db_service = DBService()
 
+SYSTEM_MESSAGE = "Given an input question and SQL response, convert it to a natural language answer. No pre-amble."
 
-def process_q1(prompt: str, result: Sequence[Row[Any]]):
-    system_message = """Given an input question and SQL response, convert it to a natural language answer. No pre-amble.
-    """
 
-    # TODO for each question, get the answer from the database and parse it
+def process_q1(result: Sequence[Row[Any]]):
+    db_context = f"Average amount is {result[0][0].quantize(Decimal('1.00'))}"
+    return db_context
+
+
+def process_q2(result: Sequence[Row[Any]]):
     db_context = result
-    human_qry_template = """
-        Input:
-        {input}
-        
-        Context from database:
-        """
-    messages = [
-        ("system", system_message),
-        ("human", human_qry_template + db_context)
-    ]
-    return ChatPromptTemplate.from_messages(messages)
-
-
-def process_q2(prompt: str, result: Sequence[Row[Any]]):
-    system_message = """Given an input question and SQL response, convert it to a natural language answer. No pre-amble.
-    """
-
-    # TODO for each question, get the answer from the database and parse it
-    db_context = result
-    human_qry_template = """
-        Input:
-        {input}
-        
-        Context from database:
-        """
-    messages = [
-        ("system", system_message),
-        ("human", human_qry_template + db_context)
-    ]
-    return ChatPromptTemplate.from_messages(messages)
+    return db_context
 
 
 class PromptService:
     def __init__(self):
         self.process_map = {
-            1: process_q1,
-            2: process_q2
+            0: process_q1,
+            1: process_q2
         }
 
-    def obtain_prompt(self, id: int, query: str, query_filter: str, prompt: str):
-        sql_query = query.format("filter", query_filter)
+    def obtain_prompt(self, id: int, query: str, query_filter: str):
+        sql_query = query.format(f"'{','.join(query_filter)}'")
         result = db_service.run_query(sql_query)
-        return self.process_map[id](prompt, result)
+        db_context = self.process_map[id](result)
+
+        human_qry_template = """
+            Input:
+            {input}
+            
+            Context from database:
+            """
+        messages = [
+            ("system", SYSTEM_MESSAGE),
+            ("human", human_qry_template + db_context)
+        ]
+        return ChatPromptTemplate.from_messages(messages)
